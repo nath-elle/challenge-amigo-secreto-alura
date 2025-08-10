0de1b158123c9a0e3f86e0f2bbf7d39d301ab1b3
Nathan Santos <nathansantos1708@gmail.com>
2025-08-10 20:31:24 -0300

    adicionar função de limpar campo input

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 let listaAmigos = [];
 
+
+// adiciona amigo na lista ao clicar no botão adicionar
 function adicionarAmigo() {
     campo = document.querySelector('input');
     if (campo.value == '') {
         alert('Por favor, insira um nome válido.');
     } else {
-        listaAmigos.push(campo.value)
+        listaAmigos.push(campo.value);
     }
+    limparCampo()
     console.log(listaAmigos)
+}
+
+
+// limpa o campo de input
+function limparCampo() {
+    campo = document.querySelector('input');
+    campo.value = '';
 }
