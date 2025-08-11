3d0bbb4ce13e4cdbd1f706d4b72fd0ac102e123b
Nathan Santos <nathansantos1708@gmail.com>
2025-08-11 20:18:44 -0300

    criar função de mostrar amigo sorteado

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let listaDeAmigos = [];
 
-// adiciona amigo na lista ao clicar no botão adicionar
+// adiciona nomes numa lista ao clicar no botão adicionar
 function adicionarAmigo() {
     campo = document.querySelector('input').value;
     if (campo == '') {
@@ -8,25 +8,38 @@ function adicionarAmigo() {
     } else {
         listaDeAmigos.push(campo);
     }
-    atualizarTextoNaTela()
-    limparCampo()
+    atualizarTextoNaTela();
+    limparCampo();
     console.log(listaDeAmigos);
 }
 
 
-// limpa o campo de input
+// limpa o campo de input de nome
 function limparCampo() {
     campo = document.querySelector('input');
     campo.value = '';
 }
 
-// adiciona elementos no html de acordo com a lista
+// cria elementos numa lista no html de acordo com o que existe no array
 function atualizarTextoNaTela() {
     let lista = document.getElementById('listaAmigos');
-    lista.innerHTML = ''
-    for (let i = 0; i <= listaDeAmigos.length; i++) {
+    lista.innerHTML = '';
+    for (let i = 0; i < listaDeAmigos.length; i++) {
         li = document.createElement('li');
         li.textContent = listaDeAmigos[(i)];
-        lista.append(li)
+        lista.append(li);
+    }
+}
+
+// sorteia um amigo na array aleatoriamente ao clicar no botao sortear
+function sortearAmigo() {
+    if (listaDeAmigos.length == 0) {
+        alert('Preencha a lista de amigos antes de sortear alguém')
+    } else {
+        let numeroIndice = parseInt(Math.random() * listaDeAmigos.length);
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = listaDeAmigos[(numeroIndice)]
+        console.log(numeroIndice);
+
     }
 }
