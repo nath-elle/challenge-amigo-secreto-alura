a9f3d85d715f7b5e16e276c2bbea0aaa59d4e996
Nathan Santos <nathansantos1708@gmail.com>
2025-08-11 18:09:42 -0300

    Criar função de atualizar lista na tela

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-let listaAmigos = [];
-
+let listaDeAmigos = [];
 
 // adiciona amigo na lista ao clicar no botão adicionar
 function adicionarAmigo() {
-    campo = document.querySelector('input');
-    if (campo.value == '') {
+    campo = document.querySelector('input').value;
+    if (campo == '') {
         alert('Por favor, insira um nome válido.');
     } else {
-        listaAmigos.push(campo.value);
+        listaDeAmigos.push(campo);
     }
+    atualizarTextoNaTela()
     limparCampo()
-    console.log(listaAmigos)
+    console.log(listaDeAmigos);
 }
 
 
@@ -18,4 +18,15 @@ function adicionarAmigo() {
 function limparCampo() {
     campo = document.querySelector('input');
     campo.value = '';
+}
+
+// adiciona elementos no html de acordo com a lista
+function atualizarTextoNaTela() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ''
+    for (let i = 0; i <= listaDeAmigos.length; i++) {
+        li = document.createElement('li');
+        li.textContent = listaDeAmigos[(i)];
+        lista.append(li)
+    }
 }
